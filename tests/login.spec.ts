f3b65f3de39beb916b92256a1be48ed8df9c6519
Zuleyma Londono <zuleyma.londono@boldcf.co>
2026-06-16 15:33:11 -0500

    Solucion reto1

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,12 +1,21 @@
 import { expect, test } from '@playwright/test'
 
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/')
+})
+
 test('Login to hrm', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+  await page.getByRole('button', { name: 'Login' }).click()
+  await expect(page).toHaveURL(/dashboard/)
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible()
+})
 
-    await page.goto('https://opensource-demo.orangehrmlive.com/')
-    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
-    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
-    await page.getByRole('button', { name: 'Login' }).click()
+test('Login to hrm invalid', async ({ page }) => {
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admi')
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin1')
+  await page.getByRole('button', { name: 'Login' }).click()
 
-    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
-
+  await expect(page.getByRole('alert')).toContainText('Invalid credentials')
 })
