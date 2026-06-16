c193109a7e2c316b79e6c1bc4aa2653cc0c4238a
Zuleyma Londono <zuleyma.londono@boldcf.co>
2026-06-16 15:01:35 -0500

    Reto 1 con el profe

---
+++ tests/login.spec.ts
@@ -0,0 +1,12 @@
+import { expect, test } from '@playwright/test'
+
+test('Login to hrm', async ({ page }) => {
+
+    await page.goto('https://opensource-demo.orangehrmlive.com/')
+    await page.getByRole('textbox', { name: 'Username' }).fill('Admin')
+    await page.getByRole('textbox', { name: 'Password' }).fill('admin123')
+    await page.getByRole('button', { name: 'Login' }).click()
+
+    await expect(page.getByRole('link', { name: 'Admin' })).toBeVisible()
+
+})
